d54931cc371b304e88c6c1b724fb9a413aa9b7c5
Jose Daniel Lara <jdlara@berkeley.edu>
2026-03-15 21:46:00 -0600

    delete reference to template in the wrong place

--- src/core/network_formulations.jl
+++ src/core/network_formulations.jl
@@ -25,8 +25,6 @@ Approximation to represent inter-area flow with each area represented as a singl
 """
 struct AreaBalancePowerModel <: AbstractActivePowerModel end
 
-# Default ProblemTemplate uses CopperPlatePowerModel (single-node network)
-IOM.ProblemTemplate() = IOM.ProblemTemplate(CopperPlatePowerModel)
 
 """
 Linear active power approximation using the power transfer distribution factor [PTDF](https://nrel-sienna.github.io/PowerNetworkMatrices.jl/stable/tutorials/tutorial_PTDF_matrix/) matrix. Balancing areas as well as synchrounous regions.
